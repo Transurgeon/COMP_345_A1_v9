85aea6458d5259530e7da1487b4adcd37d43d1f3
Nader Nasr <na_nas@live.concordia.ca>
2022-03-26 00:17:44 -0400

    Orders.h slight additions for implementations

--- Header Files/Orders.h
+++ Header Files/Orders.h
@@ -14,10 +14,15 @@ private:
 	vector<string> Order_types{ "DEPLOY", "ADVANCE", "BOMB", "BLOCKADE", "AIRLIFT", "NEGOTIATE" };
 	int orderNum;
 
+	//For implementation
+	bool isValid;
+	bool isAttavkable = true;
+
 public:
 	//Constructors, Destructors and Operators
 	Order(); 
 	Order(const Order& copy); 
+	Order(Player* player); //for implementation
 	Order& operator =(const Order& copy); 
 	~Order(); 
 	friend ostream& operator<<(ostream& output, Order& o);
@@ -27,14 +32,18 @@ public:
 	void set_orderNum(int o);
 
 	//Validates the order
-	bool validate();
+	virtual bool validate() = 0 ; //to make Order an abstract class
 
 	//Executes the order
 	virtual void execute() = 0;
+
+	//Implementation items required
+	Map* map;
+	Player* player;
 	
 };
 
-class OrdersList {
+class OrdersList: public ILoggable, public Subject { //maybe well add ILoggable stuff later
 private:
 	vector<Order*> OrderList;
 
@@ -51,24 +60,24 @@ public:
 	//add, move and remove orders in the list
 	void add(Order* order);
 	void move(int currentPos, int newPos);
-	void remove(int pos);
-	
+	void remove(Order* order);
+
+	//method from ILoggable and Subject for Observer
+	string stringToLog() override;
 };
 
 //place some armies on one of the current players territories
-class Deploy : public Order {
+class Deploy : public Order, public ILoggable, public Subject {
 private:
-	string type = "Deploy";
-	
+	Territory* targetTerritory;
+	int troopNum;
 public:
 	//attributes for executing the deploy order
 	Player* p1;
-	int t1;
-	int troopNum;
 
-	Deploy(Player* p1, int t1, int troopNum);
 	//Constructors, Destructors and Operators
 	Deploy(); 
+	Deploy(Player* p1, Territory* targetTerritory, int troopNum);
 	Deploy(const Deploy& deploy);
 	Deploy& operator=(const Deploy& deploy);
 	friend std::ostream& operator<<(std::ostream& out, const Deploy& deploy);
@@ -76,18 +85,21 @@ public:
 	~Deploy();
 
 	//Validates the order
-	bool validate();
+	virtual bool validate();
 
 	//Executes the order
-	void execute() override;
+	virtual void execute() override;
+
+	//stringTolog from observer
+	string stringToLog() override;
 
 };
 
 //move some armies from one of the current players territories (source) to an adjacent territory (target).If the target territory 
 //belongs to the current player, the armies are moved to the target territory. Else an attack happens between the two territories
-class Advance : public Order {
+class Advance : public Order, public ILoggable, public Subject {
 private:
-	string type = "Advance";
+	Territory* targetTerritory;
 	
 public:
 	//attributes for executing the advance order
@@ -111,39 +123,44 @@ public:
 	//Executes the order
 	void execute();
 
+	//stringTolog from observer
+	string stringToLog() override;
+
 };
 
 //destroy half of the armies located on an opponents territory that is adjacent to one of the current players territories
-class Bomb : public Order {
+class Bomb : public Order, public ILoggable, public Subject {
 private:
-	string type = "Bomb";
-	
+	Territory* targetTerritory;
+	Player* p1;
 public:
 	//attributes for executing the bomb order
-	Player* p1;
-	int target;
+	
 
-	Bomb(Player* p1, int target);
 	//Constructors, Destructors and Operators
 	Bomb();
 	Bomb(const Bomb& Bomb);
+	Bomb(Player* p1, Territory* targetTerritory;);
 	Bomb& operator=(const Bomb& Bomb);
 	friend std::ostream& operator<<(std::ostream& out, const Bomb& Bomb);
 	string* getType();
 	~Bomb();
 
 	//Validates the order
-	bool validate();
+	virtual bool validate();
 
 	//Executes the order
-	void execute();
+	virtual void execute();
+
+	//stringTolog from observer
+	string stringToLog() override;
 
 };
 
 //triple the number of armies on one of the current players territories and make it a neutral territory
-class Blockade : public Order {
+class Blockade : public Order, public ILoggable, public Subject {
 private:
-	string type = "Blockade";
+	Territory* targetTerritory;
 	
 public:
 	//attributes for executing the blockade order
@@ -160,17 +177,20 @@ public:
 	~Blockade();
 
 	//Validates the order
-	bool validate();
+	virtual bool validate();
 
 	//Executes the order
-	void execute();
+	virtual void execute();
+
+	//stringTolog from observer
+	string stringToLog() override;
 
 };
 
 //advance some armies from one of the current players territories to any another territory
-class AirLift : public Order {
+class AirLift : public Order, public ILoggable, public Subject {
 private:
-	string type = "AirLift";
+	Territory* targetTerritory;
 	
 public:
 	//attributes for executing the airlift order
@@ -189,17 +209,19 @@ public:
 	~AirLift();
 
 	//Validates the order
-	bool validate();
+	virtual bool validate();
 
 	//Executes the order
-	void execute();
+	virtual void execute();
 
+	//stringTolog from observer
+	string stringToLog() override;
 };
 
 //prevent attacks between the current player and another player until the end of the turn
-class Negotiate : public Order {
+class Negotiate : public Order, public ILoggable, public Subject {
 private:
-	string type = "Negotiate";
+	Territory* targetTerritory;
 	
 public:
 	//attributes for executing the negotiate order
@@ -216,10 +238,13 @@ public:
 	~Negotiate();
 
 	//Validates the order
-	bool validate();
+	virtual bool validate();
 
 	//Executes the order
-	void execute();
+	virtual void execute();
+
+	//stringTolog from observer
+	string stringToLog() override;
 	
 };
 #endif
